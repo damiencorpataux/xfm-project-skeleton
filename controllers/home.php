bced8dd2a2a33fcd5ed364a2aaacf623d3965e10
damiencorpataux <d@mien.ch>
2012-06-02 18:58:56 +0300

    HomeController cleanup

--- controllers/home.php
+++ controllers/home.php
@@ -2,10 +2,6 @@
 
 class HomeController extends xWebController {
 
-    function init() {
-        $this->add_meta('navigation/highlight', 'home');
-    }
-
     function defaultAction() {
         return xView::load('home/home')->render();
     }
