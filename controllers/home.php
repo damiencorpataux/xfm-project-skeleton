67e558957e4ea18f4dc631cb8c6329bfc8a2694b
damiencorpataux <d@mien.ch>
2013-06-20 18:08:45 +0200

    Added a load guard to prevent flooding

--- controllers/home.php
+++ controllers/home.php
@@ -9,6 +9,14 @@ class HomeController extends xWebController {
     function run_tests() {
         $dir = xContext::$basepath.'/unittests/';
         $command = "cd {$dir} && php phpunit.php units/Calculator.php";
+        // Load avg check, to prevent system overload
+        $load = sys_getloadavg();
+        if ($load[0] > 1.5) return array(
+            'command' => $command,
+            'output' => 'Server is too busy to run unittests right now. Please try again in a few minutes !',
+            'status' => 1
+        );
+        // Actual tests run
         exec($command, $output, $status);
         $output = implode("\n", $output);
         return compact('command', 'output', 'status');
